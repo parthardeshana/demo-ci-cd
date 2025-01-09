967b7ecccb9341ed9151baa8984d20b73caf12c2
pavantank2219 <ptank@technithunder.com>
2025-01-09 18:14:16 +0530

    dep test 8

--- app/page.js
+++ app/page.js
@@ -3,9 +3,8 @@ import React from "react";
 const Page = () => {
   return (
     <>
-      <h1>Main branch #DARIYOPOTE</h1>
-      <h1>showing to yashlal</h1>
-      <h1>showing to yashlal</h1>
+      <h1>Main branch</h1>
+      <h1>Commit #5</h1>
     </>
   );
 };
