d87d5eee4e7e0ad2b4744f2db53b512976610ab8
pavantank2219 <ptank@technithunder.com>
2025-01-09 18:43:39 +0530

    made demo page on main branch

--- app/page.js
+++ app/page.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const Page = () => {
   return (
-    <div class="flex h-screen w-screen items-center justify-center bg-yellow-500">
-      <h1 className="text-[50px] font-bold">Dev branch</h1>
+    <div class="flex h-screen w-screen items-center justify-center bg-purple-500">
+      <h1 className="text-[50px] font-bold">Main branch</h1>
     </div>
   );
 };
