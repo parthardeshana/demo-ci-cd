f375e877737209cc9b0a13e177f98b6c2562f23c
pavantank2219 <ptank@technithunder.com>
2025-01-09 17:37:15 +0530

    dep 3

--- app/page.js
+++ app/page.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
 
 const Page = () => {
   return (
-    <h1>Main branch C1</h1>
-  )
-}
+    <>
+      <h1>Main branch</h1>
+      <h1>Commit 09/01/2025</h1>
+    </>
+  );
+};
 
-export default Page
+export default Page;
